6a8b63f6c5f4aa2bf4504906ba41000861ae6e0a
mivium <mivium@wisebits.com>
2024-07-25 23:04:52 +0300

    step seven

---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,20 @@
+import random
+
+
+def get_rule():
+    return 'Find the greatest common divisor of given numbers.'
+
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+
+def generate_attempt_question():
+    first_number = random.randint(1, 100)
+    second_number = random.randint(1, 100)
+
+    correct_answer = gcd(first_number, second_number)
+
+    return f'Question: {first_number} {second_number}', str(correct_answer)
